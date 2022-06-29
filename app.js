69533355387306f34e52d5d2f174d33733b46078
SAHED Anis <anis.sahed.etu@univ-lille.fr>
2022-06-29 10:45:39 +0200

    attribute binding (v-bind)

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const app = Vue.createApp({
     data(){
         return {
+            url : 'https://www.google.fr',
             showBooks: true,
             title: 'Title1',
             books: [
-                { title : 'book1', author: 'author1'},
-                { title : 'book2', author: 'author2'},
-                { title : 'book2', author: 'author3'}
+                { title : 'book1', author: 'author1', img: 'assets/1.jpg'},
+                { title : 'book2', author: 'author2', img: 'assets/2.jpg'},
+                { title : 'book2', author: 'author3', img: 'assets/3.jpg'}
             ]
         }
     },
