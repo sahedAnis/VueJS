d3506bee7ef19fa3bc3744a56a29417ceb2e40ee
SAHED Anis <anis.sahed.etu@univ-lille.fr>
2022-06-29 10:19:29 +0200

    working on mouse events (@mouseover, @mouseleave, @dblclick & @mousemove)

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const app = Vue.createApp({
         return {
             showBooks: true,
             title: 'Title1',
-            age: 45
+            age: 45,
+            x : 0,
+            y : 0
         }
     },
     methods: {
         toggleShowTitle() {
             this.showBooks = !this.showBooks;
+        },
+
+        handleEvent(e, data){
+            console.log(e, e.type);
+            if (data){
+                console.log(data);
+            }
+        },
+
+        handleMousemove(e){
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 }) 
