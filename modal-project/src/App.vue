5f65993b661bb3bf440a0a1abd27266e4474e1f4
SAHED Anis <anis.sahed.etu@univ-lille.fr>
2022-06-29 14:31:59 +0200

    modal component

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <h1> {{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  components : { Modal },
   data(){
     return{
       title: 'My First Vue App!'
