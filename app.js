aa6834c783866459a839f8e51cbc2133b6c1b461
SAHED Anis <anis.sahed.etu@univ-lille.fr>
2022-06-29 10:56:31 +0200

    dynamic classes

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'Title1',
             books: [
-                { title : 'book1', author: 'author1', img: 'assets/1.jpg'},
-                { title : 'book2', author: 'author2', img: 'assets/2.jpg'},
-                { title : 'book2', author: 'author3', img: 'assets/3.jpg'}
+                { title : 'book1', author: 'author1', img: 'assets/1.jpg', isFav : true},
+                { title : 'book2', author: 'author2', img: 'assets/2.jpg', isFav : false},
+                { title : 'book2', author: 'author3', img: 'assets/3.jpg', isFav : true}
             ]
         }
     },
