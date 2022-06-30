96bcc3342336565c0c42e94727ac059f807dc516
SAHED Anis <anis.sahed.etu@univ-lille.fr>
2022-06-30 12:11:29 +0200

    finishing touches

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,10 +1,25 @@
 <template>
     <p>Reaction time : {{ this.result }} ms</p>
+    <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props: ['result']
+    props: ['result'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.result < 250) {
+            this.rank = 'Ninja Fingers';
+        } else if (this.result < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
 }
 </script>
 
@@ -16,5 +31,11 @@ export default {
         margin-right: auto;
         color: white;
         border-radius: 50px;
-}
+    }
+
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
 </style>
