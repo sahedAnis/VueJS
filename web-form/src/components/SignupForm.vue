959f356f0c1a3b04b7c513ae76ada43ce3517d8a
SAHED Anis <anis.sahed.etu@univ-lille.fr>
2022-06-30 14:23:51 +0200

    working with checkboxes

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -4,18 +4,25 @@
     <input type="email" required v-model="email">
 
     <label>Password:</label>
-    <input type="email" required v-model="password">
+    <input type="password" required v-model="password">
 
     <label>Role:</label>
     <select v-model="role">
         <option value="developer">Web Developer</option>
         <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+        <input type="checkbox" required v-model="terms">
+        <label>Accept terms and conditions</label>
+    </div>
   </form>
 
-  <p> {{ email }}</p>
-  <p> {{ password }}</p>
-  <p> {{ role }}</p>
+  <p> E-mail {{ email }}</p>
+  <p> Password {{ password }}</p>
+  <p> Role {{ role }}</p>
+  <p> Terms accepted : {{ terms }}</p>
+
 </template>
 
 <script>
@@ -24,7 +31,8 @@ export default {
         return {
             email: '',
             password: '',
-            role: ''
+            role: '',
+            terms: false,
         }
     }
 }
@@ -56,5 +64,12 @@ export default {
         border: none;
         border-bottom: 1px solid #ddd;
         color: #555;
+    }
+    input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
     }
 </style>
